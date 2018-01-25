237a4fdf6f3d146b3b619b348576e2866666f8e7
Ioannis Kozompolis <ikozompolis@velti.net>
2018-01-25 22:23:30 +0200

    Update the repositiry

--- WebApp/mycampaignpro/mycampaignpro/src/main/java/com/mycompany/mycampaignpro/Application.java
+++ WebApp/mycampaignpro/mycampaignpro/src/main/java/com/mycompany/mycampaignpro/Application.java
@@ -5,8 +5,25 @@
  */
 package com.mycompany.mycampaignpro;
 
+import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.util.Locale;
+import java.util.Map;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.MessageSourceResolvable;
+import org.springframework.context.NoSuchMessageException;
+import org.springframework.core.ResolvableType;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.Resource;
 
 /**
  *
@@ -14,8 +31,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class Application {
-
+        
+    
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        
+      
+     
     }
 }
