668d8b9b7c8c01c96d887747f8d254f1d0c5f823
Ioannis Kozompolis <ikozompolis@velti.net>
2018-02-06 22:00:38 +0200

    update code

--- WebApp/mycampaignpro/mycampaignpro/src/main/java/com/mycompany/mycampaignpro/Application.java
+++ WebApp/mycampaignpro/mycampaignpro/src/main/java/com/mycompany/mycampaignpro/Application.java
@@ -5,27 +5,10 @@
  */
 package com.mycompany.mycampaignpro;
 
-
-import java.io.IOException;
-import java.lang.annotation.Annotation;
-import java.util.Locale;
-import java.util.Map;
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.NoSuchBeanDefinitionException;
-import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationEvent;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.MessageSourceResolvable;
-import org.springframework.context.NoSuchMessageException;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.ResolvableType;
-import org.springframework.core.env.Environment;
-import org.springframework.core.io.Resource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 
