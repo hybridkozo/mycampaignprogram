97bdbc86102ab92903e1394065f5854c36373269
Ioannis Kozompolis <ikozompolis@velti.net>
2018-02-12 21:43:54 +0200

    Handle remote Message from Firebase push notification and fire new notification

--- Android/MyApplication/app/src/main/java/com/example/ikozompolis/myapplication/Firebase/MyFirebaseMessagingService.java
+++ Android/MyApplication/app/src/main/java/com/example/ikozompolis/myapplication/Firebase/MyFirebaseMessagingService.java
@@ -2,8 +2,16 @@ package com.example.ikozompolis.myapplication.Firebase;
 
 
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.util.Log;
 
+import com.example.ikozompolis.myapplication.CentralActivity;
+import com.example.ikozompolis.myapplication.R;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -26,12 +34,49 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         if (remoteMessage.getNotification() != null) {
             Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
             Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getTitle());
-            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getClickAction());
+            fireTheNotification(Integer.valueOf(remoteMessage.getMessageId()), remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
 
         }
 
         // Also if you intend on generating your own notifications as a result of a received FCM
         // message, here is where that should be initiated. See sendNotification method below.
+
+    }
+
+    public void fireTheNotification(int mNotificationId, String title, String body){
+
+        // The id of the channel.
+        String CHANNEL_ID = "my_channel_01";
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this, CHANNEL_ID)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle(title)
+                        .setContentText(body);
+        // Creates an explicit intent for an Activity in your app
+        Intent resultIntent = new Intent(this, CentralActivity.class);
+
+        // The stack builder object will contain an artificial back stack for the
+        // started Activity.
+        // This ensures that navigating backward from the Activity leads out of
+        // your app to the Home screen.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        // Adds the back stack for the Intent (but not the Intent itself)
+        stackBuilder.addParentStack(CentralActivity.class);
+        // Adds the Intent that starts the Activity to the top of the stack
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // mNotificationId is a unique integer your app uses to identify the
+        // notification. For example, to cancel the notification, you can pass its ID
+        // number to NotificationManager.cancel().
+        mNotificationManager.notify(mNotificationId, mBuilder.build());
 
     }
 
